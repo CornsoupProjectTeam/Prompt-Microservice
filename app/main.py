199a4263a58e8366dc89e0cc7e1fa3da5f700fa4
Seyeon Joo <joosy1964@gmail.com>
2025-06-01 18:50:51 +0900

    메인 실행 부분 수정정

--- app/main.py
+++ app/main.py
@@ -12,14 +12,14 @@ def start_chat_consumer():
     주감지 체인을 실행하는 함수
     """
     logging.info("주감지 체인 시작")
-    ChatConsumer()
+    ChatConsumer().start()  # ← 실행 메서드 호출 추가
 
 def start_monitor_chain():
     """
     감시 체인을 실행하는 함수
     """
     logging.info("감시 체인 시작")
-    MonitorService()
+    MonitorService().listen_for_scores()  # ← 실행 메서드 호출 추가
 
 if __name__ == "__main__":
     # 멀티 프로세싱을 이용하여 병렬로 실행
